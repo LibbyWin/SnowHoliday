acba24512ea9514c3b6f164f2f1e2f6f5580c0f4
Wini996 <libbywinfield101@gmail.com>
2019-12-09 22:31:19 +0000

    added markers so the corrolate to the places in assending order

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -9,32 +9,32 @@ function initMap() {
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var locations = [{
-        lat: 46.193646,
+        lat: 46.193646,   //france - Avariaz
         lng: 6.773301
     }, {
-        lat: 47.136266,
+        lat: 47.136266,   // autria - St. anton
         lng: 10.268162
     }, {
-        lat: 42.543433,
+        lat: 42.543433,  // Andorra - pas de la casa
         lng: 1.733681
     }, {
-        lat: 50.122180,
+        lat: 50.122180,  //Canada - Whistler
         lng: -122.957228
     }, {
         lat: 51.177105,
-        lng: -115.568315
+        lng: -115.568315  // Canada - Banff
     }, {
         lat: 39.481223,
-        lng: -106.066729
-    }, {
-        lat: 43.069520,
-        lng: 142.614962
+        lng: -106.066729  // USA - Breckenridge
     }, {
         lat: -44.872167,
-        lng: 168.948805
+        lng: 168.948805  //New Zealand - Mount Cardrona
     }, {
         lat: -44.850923,
-        lng: 168.939220
+        lng: 168.939220  // Japan - tomamu
+    }, {
+        lat: 43.069520,
+        lng: 142.614962   // New Zealand - Treble cone
     }];
 
 
