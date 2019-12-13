83d9893c9aecad9e86d41fce728165ad5860881b
Wini996 <libbywinfield101@gmail.com>
2019-12-13 13:07:15 +0000

    try for scroll top js function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ var btn = ('#scroll-top');
 
 $(window).scroll(function() {
   if ($(window).scrollTop() > 450) {
-    
-  }
-})
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }  
+});
+
