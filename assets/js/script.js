1500e3c0109eb1ea0eedf22ad347d214af74699d
Wini996 <libbywinfield101@gmail.com>
2019-12-13 13:27:01 +0000

    add JS function and all the CSS for the scrol top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,15 @@
-var btn = ('#scroll-top');
+var btn = $('#scroll-top');
 
 $(window).scroll(function() {
-  if ($(window).scrollTop() > 450) {
+  if ($(window).scrollTop() > 300) {
     btn.addClass('show');
   } else {
     btn.removeClass('show');
-  }  
+  }
 });
 
-btn.on('click', function(s) {
-  s.preventDefault();
-  $('html, body').animate({
-    scrollTop:0
-  }, '450');
-  });
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
